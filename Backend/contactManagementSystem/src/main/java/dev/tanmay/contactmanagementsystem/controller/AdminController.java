ec7870afc0bf550ca26a8b7ad19415ccd4e186ed
Tanmay <tanmaypansare708@gmail.com>
2026-06-21 07:49:21 +0530

    Added db connection

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -1,10 +1,5 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "contact_message")
-
 public class AdminController {
 
 }
--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/model/ContactMessage.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/model/ContactMessage.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "contact_message")
+@Table(name = "contact_messages")
 
 @Getter
 @Setter
@@ -27,20 +29,29 @@ public class ContactMessage {
     @Column(nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 2000)
     private String message;
 
     @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(nullable = false)
     private MessageStatus status;
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @PrePersist
     public void prePersist() {
         this.status = MessageStatus.NEW;
         this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
     }
 
 }
