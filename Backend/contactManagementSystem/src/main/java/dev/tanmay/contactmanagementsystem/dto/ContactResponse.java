4b92a7179118df35f01be52e15be01dea6cf97c6
Tanmay <tanmaypansare708@gmail.com>
2026-06-21 07:20:12 +0530

    created ContactResponse for data going out

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactResponse.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactResponse.java
@@ -1,4 +1,21 @@
 package dev.tanmay.contactmanagementsystem.dto;
 
+import dev.tanmay.contactmanagementsystem.model.MessageStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactResponse {
+    private String id;
+    private String name;
+    private String message;
+    private MessageStatus status;
+    private LocalDateTime createdAt;
 }
