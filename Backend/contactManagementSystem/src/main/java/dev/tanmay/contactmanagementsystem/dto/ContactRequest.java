3d899e3a9033e72c4e3b43021ab7c94237afb7ca
Tanmay <tanmaypansare708@gmail.com>
2026-06-21 07:13:08 +0530

    Create class for getting request with validations added validations annotations @NotBlank for username, user email and user message for me

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactRequest.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactRequest.java
@@ -1,4 +1,28 @@
 package dev.tanmay.contactmanagementsystem.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContactRequest {
+
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name too long")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Enter valid Email address")
+    private String email;
+
+    @NotBlank(message = "Message cannot be empty")
+    @Size(min = 10, max = 2000, message = "Message must be between 10 and 2000 characters")
+    private String message;
 }
