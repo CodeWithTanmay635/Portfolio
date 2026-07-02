67583f4269d299892002e6f1425cc76a46dd3b90
Tanmay <tanmaypansare708@gmail.com>
2026-07-02 21:38:41 +0530

    made String name and email as default

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactRequest.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/ContactRequest.java
@@ -1,3 +1,24 @@
 package dev.tanmay.contactmanagementsystem.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
+public record ContactRequest(
+
+        @NotBlank(message = "Name required")
+        @Size(min = 2, max = 100, message = "Name 2-100 chars")
+        String name,
+
+        @NotBlank(message = "Email required")
+        @Email(message = "Enter Valid Message")
+        String email,
+
+        @NotBlank(message = "Message required")
+        @Size(min = 10, max = 2000, message = "Message 10-2000 chars")
+        String message,
+
+        String website
+){}
