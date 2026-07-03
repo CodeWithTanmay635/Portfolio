c9de30b3f5e876bb4c8efd3b09baf2711cc66d7e
Tanmay <tanmaypansare708@gmail.com>
2026-07-03 19:40:13 +0530

    Create explaination for why this class exists in the form of comments

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/model/enums/MessageStatus.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/model/enums/MessageStatus.java
@@ -17,3 +17,16 @@ public enum MessageStatus {
         };
     }
 }
+/**
+this class is for message lifecycle which states weather the message is in NEW, READ,
+PENDING, REPLIED, & ARCHIVED
+Why do we need this class?
+because if we used String to manage message Lifecycle it would give vulnerable bugs,
+data entry typos
+ if it was done like this String status = "NEW" this may crash the system
+ because this can cause spelling mistake if we used String it may create typos in business layers
+ like sometimes it may NEW, new, New so enums make it sure for Type safety so that in DB
+ it won't be messed up with New, new, and NEW
+ Databases will gladly save whatever text string you throw at them, raw strings ruin your tracking loops.
+ An enum forces type safety.
+ **/
