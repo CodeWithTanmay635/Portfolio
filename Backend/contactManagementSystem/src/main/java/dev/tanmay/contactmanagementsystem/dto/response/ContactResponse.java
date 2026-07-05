af51c2078f0013dabec2ead2fb18347b598fc2bb
Tanmay <tanmaypansare708@gmail.com>
2026-07-05 22:04:24 +0530

    Added ContactResponse.java to Response package

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponse.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponse.java
@@ -1,4 +1,4 @@
-package dev.tanmay.contactmanagementsystem.dto;
+package dev.tanmay.contactmanagementsystem.dto.response;
 
 import dev.tanmay.contactmanagementsystem.model.enums.MessageStatus;
 import lombok.*;
