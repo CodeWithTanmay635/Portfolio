c554b5d647d8c4e9f9d993621b0ef45a569021e0
Tanmay <tanmaypansare708@gmail.com>
2026-07-07 21:29:08 +0530

    Made this class as record class

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
@@ -6,14 +6,12 @@ import lombok.*;
 import java.time.Instant;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class ContactResponseDTO {
-    private UUID id;
-    private String message;
-    private MessageStatus status;
-    private Instant createdAt;
-}
+
+public record ContactResponseDTO (
+    String referenceId,
+
+    String message,
+
+    Instant submittedAt
+    )
+{}
