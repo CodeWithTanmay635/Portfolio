1d6356bfc77edb81494777f1508aae1a41e66b0a
Tanmay <tanmaypansare708@gmail.com>
2026-07-08 12:16:45 +0530

    removed unused import files

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
@@ -1,17 +1,15 @@
 package dev.tanmay.contactmanagementsystem.dto.response;
 
-import dev.tanmay.contactmanagementsystem.model.enums.MessageStatus;
-import lombok.*;
-
 import java.time.Instant;
-import java.util.UUID;
 
 
-public record ContactResponseDTO (
+public record ContactResponseDTO(
     String referenceId,
 
     String message,
 
-    Instant submittedAt
-    )
+    Instant submittedAt,
+
+    String website
+)
 {}
