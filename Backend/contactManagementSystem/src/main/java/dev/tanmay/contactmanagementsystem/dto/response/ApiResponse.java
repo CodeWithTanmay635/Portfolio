c936b08cdba7a73784ec1c92b6119a6852b75bda
Tanmay <tanmaypansare708@gmail.com>
2026-07-09 10:26:44 +0530

    Added ApiResponse.java to make communication API

---
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ApiResponse.java
@@ -0,0 +1,12 @@
+package dev.tanmay.contactmanagementsystem.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse(
+
+) {
+}
