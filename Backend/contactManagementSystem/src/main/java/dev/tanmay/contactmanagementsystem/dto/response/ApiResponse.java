cd021fe6209f32cd78b086f6a022e9993bd57f44
Tanmay <tanmaypansare708@gmail.com>
2026-07-09 21:33:30 +0530

    added fileds for it

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ApiResponse.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ApiResponse.java
@@ -4,9 +4,19 @@ package dev.tanmay.contactmanagementsystem.dto.response;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
+import java.time.Instant;
+
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ApiResponse(
+public record ApiResponse<T>(
+        boolean success,
 
+        String message,
+
+        T data,
+
+        Instant timeStamp,
+
+        String traceId
 ) {
 }
