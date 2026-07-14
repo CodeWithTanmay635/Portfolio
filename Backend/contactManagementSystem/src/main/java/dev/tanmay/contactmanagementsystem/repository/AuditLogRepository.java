725acfb2486b1b5350026e0180af02359fe08d5a
Tanmay <tanmaypansare708@gmail.com>
2026-07-14 18:52:29 +0530

    AuditLog Database history of Status

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/AuditLogRepository.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/AuditLogRepository.java
@@ -2,7 +2,9 @@ package dev.tanmay.contactmanagementsystem.repository;
 
 import dev.tanmay.contactmanagementsystem.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuditLogRepository extends JpaRepository<Contact,Long> {
 
 }
