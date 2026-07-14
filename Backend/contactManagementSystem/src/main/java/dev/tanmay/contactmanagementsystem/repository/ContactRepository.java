3dad572bc31f9005247e02d4c65000c1af8ba798
Tanmay <tanmaypansare708@gmail.com>
2026-07-14 18:59:39 +0530

    fixed mistake off add other package in this file:

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/ContactRepository.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/ContactRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
