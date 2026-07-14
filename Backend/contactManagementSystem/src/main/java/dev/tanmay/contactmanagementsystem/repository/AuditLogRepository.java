6f9145b49382f992e793e865120f9474f56b2ffc
Tanmay <tanmaypansare708@gmail.com>
2026-07-14 19:02:24 +0530

    AuditLog Database history of Status

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/AuditLogRepository.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/repository/AuditLogRepository.java
@@ -1,6 +1,8 @@
 package dev.tanmay.contactmanagementsystem.repository;
 
 import dev.tanmay.contactmanagementsystem.model.AuditLog;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +14,15 @@ public interface AuditLogRepository extends JpaRepository<AuditLog,Long> {
 
 
     List<AuditLog> findByContactIdOrderByChangedAtDesc(UUID contactId);
+
+    Page<AuditLog> findByContactId(UUID contactId, Pageable pageable);
+
+    Page<AuditLog> findByChangeBy(String changedBy, Pageable pageable);
+
+    List<AuditLog> findByContactIdAndNewStatus(
+            UUID contactId,
+            String newStatus
+    );
+
 
 }
