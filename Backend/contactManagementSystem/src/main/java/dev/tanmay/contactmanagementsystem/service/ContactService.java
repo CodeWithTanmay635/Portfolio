4589d043ac742067a1d78e4bba7bc1db418b9a71
Tanmay <tanmaypansare708@gmail.com>
2026-07-17 15:35:19 +0530

    Created ContactRepository and EmailService instance and constructor to initialize those

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ContactService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ContactService.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService {
+    private final ContactRepository contactRepository;
+    private final EmailService emailService;
+
+    @Autowired
+    public ContactService(ContactRepository contactRepository, EmailService emailService) {
+        this.contactRepository = contactRepository;
+        this.emailService = emailService;
+    }
 }
