ee84fefe4c9b317d80d7f907f3c0f7199312b14f
Tanmay <tanmaypansare708@gmail.com>
2026-07-20 21:41:39 +0530

    added comments to fill the class

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ContactService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ContactService.java
@@ -3,10 +3,12 @@ import dev.tanmay.contactmanagementsystem.dto.request.ContactRequestDTO;
 import dev.tanmay.contactmanagementsystem.dto.response.ContactResponseDTO;
 import dev.tanmay.contactmanagementsystem.repository.ContactRepository;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+@Slf4j
 @Service
 public class ContactService {
     private final ContactRepository contactRepository;
@@ -19,7 +21,13 @@ public class ContactService {
     }
 
     @Transactional
-    public ContactResponseDTO save(ContactRequestDTO contactRequestDTO) {
+    public ContactResponseDTO save(ContactRequestDTO dto) {
+        if(dto.website() != null && !dto.website().isBlank()){
+            //do not throw exception - bot learns to retry
+            //return fake success - bot thinks it worked
+            log.warn("Bot submission detected");
+            return buildFakeResponse();
+        }
         // save contact
         // save audit log
         // both commit together
