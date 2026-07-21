d89ce9123834e2b2d3d0f55a4e5f738c9ba492e0
Tanmay <tanmaypansare708@gmail.com>
2026-07-21 12:46:00 +0530

    made some changes in form method:

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/dto/response/ContactResponseDTO.java
@@ -1,13 +1,25 @@
 package dev.tanmay.contactmanagementsystem.dto.response;
 
+import dev.tanmay.contactmanagementsystem.model.Contact;
+
 import java.time.Instant;
+import java.util.UUID;
 
 
 public record ContactResponseDTO(
-    String referenceId,
+    UUID referenceId,
 
-    Instant submittedAt,
+    String confirmationMessage,
 
-    String website
+    Instant submittedAt
 )
-{}
+{
+
+    public static ContactResponseDTO from(Contact contact){
+        return new ContactResponseDTO(
+               contact.getId(),
+                "Message Received. Will Respond Shortly!!",
+                contact.getCreatedAt()
+        );
+    }
+}
