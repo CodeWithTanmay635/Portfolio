6e9dc0b29434a9fdff56603da0e3aeff001acbd9
Tanmay <tanmaypansare708@gmail.com>
2026-07-28 21:48:25 +0530

    Created class to estimate priority of the message

---
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/PriorityEstimatorService.java
@@ -0,0 +1,12 @@
+package dev.tanmay.contactmanagementsystem.service;
+
+
+import dev.tanmay.contactmanagementsystem.dto.request.ContactRequestDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PriorityEstimatorService {
+    public int calculateScore(ContactRequestDTO contactResponseDTO) {
+        return 0;
+    }
+}
