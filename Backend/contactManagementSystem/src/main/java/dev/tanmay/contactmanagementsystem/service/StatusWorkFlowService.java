faf1b45331ca58e96077a10afd9f6e94e6671eeb
Tanmay <tanmaypansare708@gmail.com>
2026-07-29 13:40:43 +0530

    added dependencies

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/StatusWorkFlowService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/StatusWorkFlowService.java
@@ -1,4 +1,34 @@
 package dev.tanmay.contactmanagementsystem.service;
 
+import dev.tanmay.contactmanagementsystem.dto.request.StatusUpdateRequest;
+import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
+import dev.tanmay.contactmanagementsystem.repository.AuditLogRepository;
+import dev.tanmay.contactmanagementsystem.repository.ContactRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
 public class StatusWorkFlowService {
+
+    private final ContactRepository contactRepository;
+    private final AuditLogRepository auditLogRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public StatusWorkFlowService(
+            ContactRepository contactRepository,
+            AuditLogRepository auditLogRepository,
+            ApplicationEventPublisher eventPublisher) {
+        this.contactRepository = contactRepository;
+        this.auditLogRepository = auditLogRepository;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public ApiResponse<Void> updateStatus(
+            UUID contactId,
+            StatusUpdateRequest request
+    ){
+     return  null;
+    }
 }
