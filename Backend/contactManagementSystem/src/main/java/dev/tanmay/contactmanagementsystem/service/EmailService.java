37044887fe4ecb86cff5f90177008351ce8acf14
Tanmay <tanmaypansare708@gmail.com>
2026-08-03 12:43:02 +0530

    Added a method as sendReply()

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/EmailService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/EmailService.java
@@ -1,11 +1,15 @@
 package dev.tanmay.contactmanagementsystem.service;
 
+import dev.tanmay.contactmanagementsystem.dto.request.ReplyRequest;
+import dev.tanmay.contactmanagementsystem.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class EmailService {
 
@@ -19,6 +23,9 @@ public class EmailService {
         this.mailSender = mailSender;
     }
 
+    public void sendReply(Contact contact, ReplyRequest replyRequest) {
+        log.info("Sending email to contact with id {} subject: {}", contact.getId(), contact.getMessage());
+    }
     public void sendNewMessageNotification(String visitorName, String visitorEmail, String visitorMessage) {
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo(notificationEmail);
