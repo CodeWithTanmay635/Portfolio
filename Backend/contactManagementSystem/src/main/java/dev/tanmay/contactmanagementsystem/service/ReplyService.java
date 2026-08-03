fdf2bbb9fbf6eaa83ad8671ab620a8dd2e0edf81
Tanmay <tanmaypansare708@gmail.com>
2026-08-03 11:22:23 +0530

    created helper method for finding contact

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ReplyService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/ReplyService.java
@@ -1,11 +1,16 @@
 package dev.tanmay.contactmanagementsystem.service;
 
-import dev.tanmay.contactmanagementsystem.dto.response.ContactResponseDTO;
+import dev.tanmay.contactmanagementsystem.dto.request.ReplyRequest;
+import dev.tanmay.contactmanagementsystem.dto.response.AdminContactResponseDTO;
+import dev.tanmay.contactmanagementsystem.exception.ContactNotFoundException;
+import dev.tanmay.contactmanagementsystem.model.Contact;
 import dev.tanmay.contactmanagementsystem.repository.AuditLogRepository;
 import dev.tanmay.contactmanagementsystem.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -27,4 +32,16 @@ public class ReplyService {
         this.eventPublisher = eventPublisher;
     }
 
+    public AdminContactResponseDTO replyToContact(
+            UUID id,
+            ReplyRequest  replyRequest
+    ){
+        return  null;
+    }
+
+    private Contact findContact(UUID id) {
+        return contactRepository
+                .findById(id).orElseThrow(() ->
+                       new ContactNotFoundException(id));
+    }
 }
