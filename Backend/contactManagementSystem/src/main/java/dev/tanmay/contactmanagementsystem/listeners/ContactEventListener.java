36e4a6aecc5005a96fae71bcdc5835f82cdaec0f
Tanmay <tanmaypansare708@gmail.com>
2026-08-04 12:55:04 +0530

    injected some dependencies

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/listeners/ContactEventListener.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/listeners/ContactEventListener.java
@@ -1,4 +1,38 @@
 package dev.tanmay.contactmanagementsystem.listeners;
 
+import dev.tanmay.contactmanagementsystem.event.ContactReceivedEvent;
+import dev.tanmay.contactmanagementsystem.event.ContactRepliedEvent;
+import dev.tanmay.contactmanagementsystem.service.EmailService;
+import dev.tanmay.contactmanagementsystem.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
 public class ContactEventListener {
+
+    private final NotificationService notificationService;
+    private final EmailService emailService;
+
+    public ContactEventListener(
+                                NotificationService notificationService,
+                                EmailService emailService) {
+        this.notificationService = notificationService;
+        this.emailService = emailService;
+    }
+
+    @Async
+    @EventListener
+    public void handleContactReceived(ContactReceivedEvent event) {
+
+    }
+
+    @Async
+    @EventListener
+    public void handleContactReplied(ContactRepliedEvent event) {
+
+    }
 }
