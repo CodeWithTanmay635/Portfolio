ef352ab1009765c22b8dc0b451c910785b3eeac5
Tanmay <tanmaypansare708@gmail.com>
2026-08-05 11:48:57 +0530

    added exeption handler

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/exception/GlobalExceptionHandler.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/exception/GlobalExceptionHandler.java
@@ -1,7 +1,35 @@
 package dev.tanmay.contactmanagementsystem.exception;
 
-public class GlobalExceptionHandler extends RuntimeException {
-    public GlobalExceptionHandler(String message) {
-        super(message);
-    }
+import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+  @ExceptionHandler(AlreadyRepliedException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiResponse<Void> handleAlreadyReplied(AlreadyRepliedException ex){
+      log.info(ex.getMessage());
+      return ApiResponse.error(ex.getMessage());
+  }
+
+  @ExceptionHandler(DuplicateContactException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse<Void> handleDuplicateContact(DuplicateContactException ex){
+      log.info(ex.getMessage());
+      return ApiResponse.error(ex.getMessage());
+  }
+
+  @ExceptionHandler(ContactNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiResponse<Void> handleNotFound(ContactNotFoundException ex){
+      log.info(ex.getMessage());
+      return ApiResponse.error(ex.getMessage());
+  }
+
+
 }
