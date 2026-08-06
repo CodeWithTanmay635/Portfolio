ff283eb95f2ca850cf18f22a99d43e5c50aea540
Tanmay <tanmaypansare708@gmail.com>
2026-08-06 19:26:10 +0530

    added Method Argument Not Valid Excetion

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/exception/GlobalExceptionHandler.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/exception/GlobalExceptionHandler.java
@@ -3,9 +3,13 @@ package dev.tanmay.contactmanagementsystem.exception;
 import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -51,6 +55,18 @@ public class GlobalExceptionHandler{
     public ApiResponse<Void> handleMessageNotFound(MessageNotFoundException ex){
       log.warn("Message Not found : {}",ex.getMessage());
       return ApiResponse.error(ex.getMessage());
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public ApiResponse<Void> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+      String errors = ex.getBindingResult()
+              .getFieldErrors()
+              .stream()
+              .map(FieldError::getDefaultMessage)
+              .collect(Collectors.joining(", "));
+      log.warn("Invalid Request : {}",errors);
+      return ApiResponse.error(errors);
   }
 
  @ExceptionHandler(Exception.class)
