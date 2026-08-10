e94af0a64746dc594d7cc673551462630649fc61
Tanmay <tanmaypansare708@gmail.com>
2026-08-10 22:21:02 +0530

    added dependencies as ContactRepository contactRepository and a helper method as getAllContacts

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/AdminService.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/service/AdminService.java
@@ -1,9 +1,30 @@
 package dev.tanmay.contactmanagementsystem.service;
 
+import dev.tanmay.contactmanagementsystem.dto.response.AdminContactResponseDTO;
+import dev.tanmay.contactmanagementsystem.model.Contact;
+import dev.tanmay.contactmanagementsystem.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class AdminService {
+
+    private final ContactRepository contactRepository;
+
+    public AdminService(ContactRepository contactRepository) {
+        this.contactRepository = contactRepository;
+    }
+
+    public Page<AdminContactResponseDTO> getAllContacts(
+            int pageNumber, int size, String sortBy) {
+        Pageable pageable = PageRequest.of(pageNumber, size, Sort.by(sortBy));
+
+        return contactRepository.findAll(pageable)
+                .map(AdminContactResponseDTO :: from);
+    }
 }
