a1804ca07a4874088a1d09c1e8eb4d4d9fffe59c
Tanmay <tanmaypansare708@gmail.com>
2026-08-11 22:14:31 +0530

    Just realized that we don't need AdminService

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/ContactController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/ContactController.java
@@ -1,6 +1,5 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
-import dev.tanmay.contactmanagementsystem.service.AdminService;
 import dev.tanmay.contactmanagementsystem.service.ContactService;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,14 +8,11 @@ import org.springframework.web.bind.annotation.*;
 public class ContactController {
 
     private final ContactService contactService;
-    private final AdminService adminService;
 
     public ContactController(
             ContactService contactService,
-            AdminService adminService
     ) {
         this.contactService = contactService;
-        this.adminService = adminService;
     }
     @PostMapping
     public String submitContact() {
