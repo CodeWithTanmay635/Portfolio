9c1b15018afff68a8229395dd0601981cea3fc75
Tanmay <tanmaypansare708@gmail.com>
2026-08-12 22:47:21 +0530

    added submitContact() method in the class

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/ContactController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/ContactController.java
@@ -1,8 +1,16 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
+import dev.tanmay.contactmanagementsystem.dto.request.ContactRequestDTO;
+import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
+import dev.tanmay.contactmanagementsystem.dto.response.ContactResponseDTO;
 import dev.tanmay.contactmanagementsystem.service.ContactService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/contact")
 public class ContactController {
@@ -15,8 +23,21 @@ public class ContactController {
         this.contactService = contactService;
     }
     @PostMapping
-    public String submitContact() {
-        return "Contact API Working!";
+    public ResponseEntity<ApiResponse<ContactResponseDTO>> submitContact(
+            @RequestBody ContactRequestDTO dto,
+            HttpServletRequest request){
+        log.info("Received request to submit contact request IP {}",
+                request.getRemoteAddr());
+
+        ContactResponseDTO response = contactService.submitContact(
+                dto
+        );
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.success(
+                        "Message received successfully",
+                        response
+                ));
     }
 
 }
