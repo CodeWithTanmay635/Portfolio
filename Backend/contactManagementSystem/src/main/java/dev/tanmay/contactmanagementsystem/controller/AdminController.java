d1d01c15827185153ede64c4007422525babe838
Tanmay <tanmaypansare708@gmail.com>
2026-08-12 22:40:43 +0530

    Added some critical steps

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -1,5 +1,33 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
+import dev.tanmay.contactmanagementsystem.dto.request.ContactRequestDTO;
+import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
+import dev.tanmay.contactmanagementsystem.dto.response.ContactResponseDTO;
+import dev.tanmay.contactmanagementsystem.service.AdminService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/admin")
 public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(
+            AdminService adminService
+    ){
+        this.adminService = adminService;
+    }
+
+    @PostMapping
+    ResponseEntity<ApiResponse<ContactResponseDTO>> submitContact(
+            @RequestBody ContactRequestDTO dto,
+            HttpServletRequest request){
+        log.info
+    }
 
 }
