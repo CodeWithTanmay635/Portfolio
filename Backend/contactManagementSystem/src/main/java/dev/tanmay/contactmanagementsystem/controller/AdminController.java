8ee41d60546564b03ba4744fb1272f87270525eb
Tanmay <tanmaypansare708@gmail.com>
2026-08-14 22:39:59 +0530

    Removed almost everything

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -1,13 +1,6 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
-import dev.tanmay.contactmanagementsystem.dto.request.ContactRequestDTO;
-import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
-import dev.tanmay.contactmanagementsystem.dto.response.ContactResponseDTO;
 import dev.tanmay.contactmanagementsystem.service.AdminService;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
