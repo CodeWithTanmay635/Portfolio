19127f7f251d9ea7f8f7359d1c70c1932a02ad8a
Tanmay <tanmaypansare708@gmail.com>
2026-08-14 22:49:31 +0530

    not able to find the answers

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -1,10 +1,21 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
+import dev.tanmay.contactmanagementsystem.dto.response.AdminContactResponseDTO;
+import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
 import dev.tanmay.contactmanagementsystem.service.AdminService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/admin")
 public class AdminController {
@@ -14,5 +25,23 @@ public class AdminController {
             AdminService adminService
     ){
         this.adminService = adminService;
+    }
+    @GetMapping
+    public ResponseEntity<ApiResponse<AdminContactResponseDTO>> getAllContacts(
+            @RequestBody AdminContactResponseDTO dto,
+            HttpServletResponse response){
+        log.info("Getting all contacts from admin");
+
+        AdminContactResponseDTO responseDTO = adminService.getAllContacts(
+                20,
+                20,
+                "all contacts"
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        ""
+                ))
     }
 }
