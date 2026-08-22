88d5e280fd96ca8cce492e97d2fcec1dcabb0143
Tanmay <tanmaypansare708@gmail.com>
2026-08-22 19:29:40 +0530

    created getAllContacts() method

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -2,46 +2,57 @@ package dev.tanmay.contactmanagementsystem.controller;
 
 import dev.tanmay.contactmanagementsystem.dto.response.AdminContactResponseDTO;
 import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
+import dev.tanmay.contactmanagementsystem.dto.response.PagedResponse;
+import dev.tanmay.contactmanagementsystem.model.enums.MessagePriority;
+import dev.tanmay.contactmanagementsystem.model.enums.MessageStatus;
 import dev.tanmay.contactmanagementsystem.service.AdminService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import dev.tanmay.contactmanagementsystem.service.ReplyService;
+import dev.tanmay.contactmanagementsystem.service.StatusWorkFlowService;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @Slf4j
 @RestController
-@RequestMapping("api/v1/admin")
+@RequestMapping("api/v1/admin/contacts")
 public class AdminController {
     private final AdminService adminService;
+    private final ReplyService replyService;
+    private final StatusWorkFlowService status;
 
     public AdminController(
-            AdminService adminService
+            AdminService adminService,
+            ReplyService replyService,
+            StatusWorkFlowService status
     ){
         this.adminService = adminService;
+        this.replyService = replyService;
+        this.status = status;
     }
-    @GetMapping
-    public ResponseEntity<ApiResponse<AdminContactResponseDTO>> getAllContacts(
-            @RequestBody AdminContactResponseDTO dto,
-            HttpServletResponse response){
-        log.info("Getting all contacts from admin");
 
-        AdminContactResponseDTO responseDTO = adminService.getAllContacts(
-                20,
-                20,
-                "all contacts"
+    @GetMapping("{id}")
+    public ResponseEntity<ApiResponse<PagedResponse<AdminContactResponseDTO>>> get(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size,
+        @RequestParam(defaultValue = "createdAt") String sortBy,
+        @RequestParam(required = false)MessageStatus status,
+        @RequestParam(required = false)MessagePriority priority
+    ){
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+
+        PagedResponse<AdminContactResponseDTO> response =
+                adminService.getAllContacts(
+                        status,
+                        priority,
+                        pageable
+                );
+        return ResponseEntity.ok(
+                ApiResponse.success("Messages fetched", response)
         );
-
-        return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(ApiResponse.success(
-                        ""
-                ))
     }
+
 }
