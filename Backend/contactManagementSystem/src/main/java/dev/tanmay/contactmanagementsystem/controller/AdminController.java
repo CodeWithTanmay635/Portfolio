070d98fddf2ed613ca67cf861801e78ec9cd6c34
Tanmay <tanmaypansare708@gmail.com>
2026-08-23 17:42:18 +0530

    added methods for reply, updateStatus and delete status

--- Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
+++ Backend/contactManagementSystem/src/main/java/dev/tanmay/contactmanagementsystem/controller/AdminController.java
@@ -1,5 +1,7 @@
 package dev.tanmay.contactmanagementsystem.controller;
 
+import dev.tanmay.contactmanagementsystem.dto.request.ReplyRequest;
+import dev.tanmay.contactmanagementsystem.dto.request.StatusUpdateRequest;
 import dev.tanmay.contactmanagementsystem.dto.response.AdminContactResponseDTO;
 import dev.tanmay.contactmanagementsystem.dto.response.ApiResponse;
 import dev.tanmay.contactmanagementsystem.dto.response.PagedResponse;
@@ -8,12 +10,15 @@ import dev.tanmay.contactmanagementsystem.model.enums.MessageStatus;
 import dev.tanmay.contactmanagementsystem.service.AdminService;
 import dev.tanmay.contactmanagementsystem.service.ReplyService;
 import dev.tanmay.contactmanagementsystem.service.StatusWorkFlowService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 
 @Slf4j
@@ -22,20 +27,20 @@ import org.springframework.web.bind.annotation.*;
 public class AdminController {
     private final AdminService adminService;
     private final ReplyService replyService;
-    private final StatusWorkFlowService status;
+    private final StatusWorkFlowService statusWorkFlowService;
 
     public AdminController(
             AdminService adminService,
             ReplyService replyService,
-            StatusWorkFlowService status
+            StatusWorkFlowService statusWorkFlowService
     ){
         this.adminService = adminService;
         this.replyService = replyService;
-        this.status = status;
+        this.statusWorkFlowService = statusWorkFlowService;
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<ApiResponse<PagedResponse<AdminContactResponseDTO>>> get(
+    @GetMapping("/messages")
+    public ResponseEntity<ApiResponse<PagedResponse<AdminContactResponseDTO>>> getAllMessages(
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size,
         @RequestParam(defaultValue = "createdAt") String sortBy,
@@ -52,6 +57,56 @@ public class AdminController {
                 );
         return ResponseEntity.ok(
                 ApiResponse.success("Messages fetched", response)
+        );
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<ApiResponse<AdminContactResponseDTO>> getById(
+            @PathVariable UUID id){
+        AdminContactResponseDTO response = adminService.getById(id);
+        return ResponseEntity.ok(
+                ApiResponse.success("Message fetched", response)
+        );
+    }
+
+    @PatchMapping("/message/{id}/status")
+    public ResponseEntity<ApiResponse<AdminContactResponseDTO>> updateStatus(
+            @PathVariable UUID id,
+            @Valid @RequestBody StatusUpdateRequest dto
+            ){
+        log.info("Status update - ID: {} -> {}", id, dto.newStatus());
+
+        AdminContactResponseDTO response =
+                statusWorkFlowService.updateStatus(id,dto);
+
+        return ResponseEntity.ok(
+                ApiResponse.success("Message updated", response)
+        );
+    }
+
+    @PostMapping("/message/{id}/reply")
+    public ResponseEntity<ApiResponse<AdminContactResponseDTO>> reply(
+            @PathVariable UUID id,
+            @Valid @RequestBody ReplyRequest dto
+    ){
+        log.info("Reply - sent: {}", id);
+
+        AdminContactResponseDTO response =
+                replyService.replyToContact(id, dto);
+
+        return ResponseEntity.ok(
+                ApiResponse.success("Message replied", response)
+        );
+    }
+
+    @DeleteMapping("/message/{id}")
+    public ResponseEntity<ApiResponse<Void>> delete(
+            @PathVariable UUID id){
+        log.info("Delete - ID: {}", id);
+        adminService.delete(id);
+
+        return ResponseEntity.ok(
+                ApiResponse.success("Message deleted", null)
         );
     }
 
